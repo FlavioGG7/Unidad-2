c5a3b7260d7031d381af6ad8bd0d3e5ebdb2fead
FlavioGG7 <flaviogallardoglz@gmail.com>
2026-09-23 17:28:22 -0600

    Delete promedio.py

--- promedio.py
+++
@@ -1,5 +0,0 @@
-N1 = float(input("Ingresa el primer numero: "))
-N2 = float(input("Ingresa el segundo numero: "))
-N3 = float(input("Ingresa el tercer numero: "))
-N4 = (N1 + N2 + N3) / 3
-print("El promedio de los tres numeros es: ", N4)
