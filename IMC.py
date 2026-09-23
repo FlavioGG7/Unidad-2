ffc4156be8d4f716c26aa260c85f421bff983c27
FlavioGG7 <flaviogallardoglz@gmail.com>
2026-09-23 17:28:13 -0600

    Delete IMC.py

--- IMC.py
+++
@@ -1,4 +0,0 @@
-peso = float (input ("Ingresa tu peso en kilogramos: "))
-altura = float (input ("Ingresa tu altura en metros: "))
-Imc = peso / (altura * altura)
-print ("Tu índice de masa corporal es: ", Imc)
