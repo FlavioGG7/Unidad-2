3075a49cfc41532c863ff192d13f9bd7482ee97f
FlavioGG7 <flaviogallardoglz@gmail.com>
2026-09-23 16:44:53 -0600

    Add files via upload

---
+++ rect.py
@@ -0,0 +1,5 @@
+
+lado1 = float(input("Ingrese el valor del primer lado del rectángulo: "))
+lado2 = float(input("Ingrese el valor del segundo lado del rectángulo: ")) 
+area = lado1 * lado2
+print("El área del rectángulo es:", area)
