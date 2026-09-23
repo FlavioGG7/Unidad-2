f82e5c80eba002e16f95ff4044690e359c23e655
FlavioGG7 <flaviogallardoglz@gmail.com>
2026-09-23 17:39:59 -0600

    Add files via upload

---
+++ SALARIO.py
@@ -0,0 +1,7 @@
+#programa: Este programa calcula el salario neto de un empleado después de aplicar impuestos y deducciones.
+salariobm = float (input("Ingresa tu salario bruto mensual: "))
+Porcentaje = 16
+deducciones = float (input("Ingresa el monto de deducciones adicionales: "))
+impuesto = salariobm * (Porcentaje / 100)
+Salario_neto = salariobm - impuesto - deducciones
+print("Tu salario neto es: ", Salario_neto)
