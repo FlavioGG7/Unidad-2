de8040bd3aee9f82f045778590a08470a30a45f6
FlavioGG7 <flaviogallardoglz@gmail.com>
2026-09-23 16:56:47 -0600

    Add files via upload

---
+++ IMC.py
@@ -0,0 +1,4 @@
+peso = float (input ("Ingresa tu peso en kilogramos: "))
+altura = float (input ("Ingresa tu altura en metros: "))
+Imc = peso / (altura * altura)
+print ("Tu índice de masa corporal es: ", Imc)
